90ef8323d6f0528f3539e58e2fd545cb671d9457
earthrockey <ratchapong5924@gmail.com>
2019-01-07 19:56:40 +0700

    add fild DataOlder -close #17

---
+++ backend/src/main/java/com/se/team19/server/Entity/DataOlder.java
@@ -0,0 +1,21 @@
+package th.sut.sa.demo.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "DataOlder")
+public class DataOlder {
+    @Id
+    @SequenceGenerator(name = "dataolder_seq", sequenceName = "dataolder_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dataolder_seq")
+    @Column(name = "Dataolder_ID", unique = true, nullable = false)
+    private @NonNull Long id;
+    private @NonNull String oldername;
+    private @NonNull Date olderbirth;
+    private @NonNull String parentphone;
+
+
+}
